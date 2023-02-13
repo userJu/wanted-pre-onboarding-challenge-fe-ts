969e205c4b1834e5ee9e9853e583e193b44f6d4b
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-13 18:51:56 +0900

    Feat: 설계를 바탕으로 구현

--- src/index.js
+++ src/index.js
@@ -1,2 +1,69 @@
 "use strict";
-let hello = "hello world";
+// Class representing a TodoItem.
+class TodoItem {
+    // CREATE Methods ...
+    constructor(userId, content, category, tags) {
+        this.userId = userId;
+        this.content = content;
+        this.category = category;
+        this.tags = tags;
+    }
+    // UPDATE Methods...
+    updateContent(content) {
+        this.content = content;
+    }
+    updateCategory(category) {
+        this.category = category;
+    }
+    updateTag(tagId, tagName) {
+        var _a;
+        this.tags = (_a = this.tags) === null || _a === void 0 ? void 0 : _a.map((tag) => tag.tagId === tagId ? Object.assign(Object.assign({}, tag), { tagName }) : tag);
+    }
+    // DELETE Methods...
+    deleteTag(tagId) {
+        var _a;
+        this.tags = (_a = this.tags) === null || _a === void 0 ? void 0 : _a.filter((tag) => tag.tagId !== tagId);
+    }
+    deleteTagAll() {
+        this.tags = undefined;
+    }
+    // READ Methods...
+    render(parentNode) {
+        parentNode.innerHTML = `
+      <li>${(this.userId, this.category, this.content, this.tags)}</li>
+    `;
+    }
+}
+class TodoList {
+    constructor() {
+        this.todoListData = []; // todoList []로 초기화
+    }
+    // CREATE Methods...
+    addTodoItem(userId, content, category, tags) {
+        this.todoListData = [
+            ...this.todoListData,
+            {
+                id: this.todoListData.length + 1,
+                todoItem: new TodoItem(userId, content, category, tags),
+                // error
+                // Argument of type 'TagData[] | undefined' is not assignable to parameter of type
+                // 'TagData[]'. Type 'undefined' is not assignable to type 'TagData[]'.
+            },
+        ];
+    }
+    // DELETE Methods...
+    deleteTodoItem(userId, todoId) {
+        this.todoListData = this.todoListData.filter((todo) => todo.id !== todoId && todo.todoItem.userId !== userId);
+    }
+    deleteTodoList(userId) {
+        this.todoListData = this.todoListData.filter((todo) => todo.todoItem.userId !== userId);
+    }
+    // READ Methods...
+    filterTodoListByUser(userId) {
+        const filteredTodoList = this.todoListData.filter((todo) => todo.todoItem.userId === userId);
+        return filteredTodoList;
+    }
+    render() {
+        // ...
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,110 @@
-let hello: string = "hello world";
+interface TagData {
+  tagId: number;
+  tagName: string;
+}
+
+// Class representing a TodoItem.
+class TodoItem {
+  userId: string; // - The id of User.
+  content: string; // - The description of todoItem.
+  category: string; // - The category of todoItem.
+  tags?: TagData[]; // - The tagInfo List of todoItem, this is optional parameter
+
+  // CREATE Methods ...
+  constructor(
+    userId: string,
+    content: string,
+    category: string,
+    tags: TagData[]
+  ) {
+    this.userId = userId;
+    this.content = content;
+    this.category = category;
+    this.tags = tags;
+  }
+
+  // UPDATE Methods...
+  updateContent(content: string) {
+    this.content = content;
+  }
+  updateCategory(category: string) {
+    this.category = category;
+  }
+  updateTag(tagId: number, tagName: string) {
+    this.tags = this.tags?.map((tag) =>
+      tag.tagId === tagId ? { ...tag, tagName } : tag
+    );
+  }
+
+  // DELETE Methods...
+  deleteTag(tagId: number) {
+    this.tags = this.tags?.filter((tag) => tag.tagId !== tagId);
+  }
+  deleteTagAll() {
+    this.tags = undefined;
+  }
+
+  // READ Methods...
+  render(parentNode: HTMLElement) {
+    parentNode.innerHTML = `
+      <li>${(this.userId, this.category, this.content, this.tags)}</li>
+    `;
+  }
+}
+
+interface TodoItemData {
+  id: number;
+  todoItem: TodoItem;
+}
+
+class TodoList {
+  todoListData: TodoItemData[];
+
+  constructor() {
+    this.todoListData = []; // todoList []로 초기화
+  }
+
+  // CREATE Methods...
+  addTodoItem(
+    userId: string,
+    content: string,
+    category: string,
+    tags?: TagData[]
+  ) {
+    this.todoListData = [
+      ...this.todoListData,
+      {
+        id: this.todoListData.length + 1,
+        todoItem: new TodoItem(userId, content, category, tags),
+        // error
+        // Argument of type 'TagData[] | undefined' is not assignable to parameter of type
+        // 'TagData[]'. Type 'undefined' is not assignable to type 'TagData[]'.
+      },
+    ];
+  }
+
+  // DELETE Methods...
+  deleteTodoItem(userId: string, todoId: number) {
+    this.todoListData = this.todoListData.filter(
+      (todo) => todo.id !== todoId && todo.todoItem.userId !== userId
+    );
+  }
+
+  deleteTodoList(userId: string) {
+    this.todoListData = this.todoListData.filter(
+      (todo) => todo.todoItem.userId !== userId
+    );
+  }
+
+  // READ Methods...
+  filterTodoListByUser(userId: string): TodoItemData[] {
+    const filteredTodoList = this.todoListData.filter(
+      (todo) => todo.todoItem.userId === userId
+    );
+    return filteredTodoList;
+  }
+
+  render() {
+    // ...
+  }
+}
